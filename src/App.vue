a9a7c350862b30badac54f7e5a93ab583868d843
amiller <adam5bucks475@gmail.com>
2021-10-02 14:45:38 -0400

    Add modal component.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button v-if="!isOpen" class="modal-button" @click="toggleModal">
+      Open
+    </button>
+    <button v-else class="modal-button" @click="toggleModal">Close</button>
+  </div>
+  <p>test</p>
+    <SimpleModal v-bind:modalOpen="isOpen">
+      <h1>Test</h1>
+      <p>using a slot to add content to the modal.</p>
+    </SimpleModal>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SimpleModal from "./components/modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggleModal: function () {
+      this.isOpen = !this.isOpen;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    SimpleModal,
+  },
+};
 </script>
 
 <style>
@@ -22,5 +42,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.modal-button {
+  background-image: linear-gradient(144deg, #00ffb3, #9ae60e 50%, #00ffb3);
+  border: 0;
+  border-radius: 8px;
+  box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+  box-sizing: border-box;
+  color: #ffffff;
+  font-family: Phantomsans, sans-serif;
+  font-size: 20px;
+  line-height: 1em;
+  max-width: 100%;
+  min-width: 140px;
+  padding: 19px 24px;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  cursor: pointer;
+}
+
+.modal-button:active,
+.modal-button:hover {
+  outline: 0;
+}
+
+@media (min-width: 768px) {
+  .modal-button {
+    font-size: 24px;
+    min-width: 196px;
+  }
 }
 </style>
---
+++ src/components/modal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="modal-container"> 
+    <transition name="fade">
+      <div v-if="modalOpen" class="modal">
+        <slot></slot>
+      </div>
+    </transition>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props:{
+        modalOpen: {
+            type: Boolean,
+            required: false
+        }
+}
+};
+</script>
+<style scoped>
+
+
+.modal-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 10em;
+  position: fixed;
+  top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    
+}
+
+.modal {
+  width: 50em;
+  height: 25em;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em,
+    rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em,
+    rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
+}
+
+.fade-enter-from {
+    opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active{
+    transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
